a28f8fc0e26c4065b12c5819e4ac842d6a61f0e4
jro <julian.roggendorf@rub.de>
2020-12-03 10:59:39 +0100

    Created functions to generate match plan

--- generateMatchPlan.js
+++ generateMatchPlan.js
@@ -1,42 +1,53 @@
-function calculateMatchPlan() {
+function generateMatchPlan() {
+  // Check if there is an old match plan present
   let oldTable = document.getElementById("matchListTable");
   if (oldTable) {
     oldTable.remove();
   }
+
+  // Get values from inputs
   let matchListTable = document.createElement("TABLE");
-  matchListTable.setAttribute("id", "matchListTable");
   let playerList = document.getElementById("playerList");
-  playerListItems = playerList.getElementsByTagName("li");
   let matchWeekCount = document.getElementById("matchWeekCount").valueAsNumber;
-  document.body.appendChild(matchListTable);
-  let nameRow = matchListTable.insertRow(0);
-  let startDate = new Date(document.getElementById("startDate").value);
-  let dateCell = nameRow.insertCell(0);
-  let text = document.createTextNode("Date");
-  dateCell.appendChild(text);
-  let playersPerMatch = 4;
   let courtCount = document.getElementById("courtCount").valueAsNumber;
   let subCount = document.getElementById("subCount").valueAsNumber;
-  let participantsPerMatch = playersPerMatch*courtCount + subCount;
 
+  // Generate match dates dateArray
   let weekDayArray = getSelectedWeekDays();
+  let startDate = new Date(document.getElementById("startDate").value);
   let dateArray = getDateArray(startDate, weekDayArray, matchWeekCount);
 
+  // Participants calculation
+  let playersPerMatch = 4;
+  let participantsPerMatchDay = playersPerMatch*courtCount + subCount;
+
+  // Setup matchListTable
+  let nameRow = matchListTable.insertRow(0);
+  let dateCell = nameRow.insertCell(0);
+  let text = document.createTextNode("Date");
+  matchListTable.setAttribute("id", "matchListTable");
+  playerListItems = playerList.getElementsByTagName("li");
+  document.body.appendChild(matchListTable);
+  dateCell.appendChild(text);
   for (i = 0; i < playerListItems.length; i++) {
     cell = nameRow.insertCell(i + 1);
     text = document.createTextNode(playerListItems[i].textContent);
     cell.appendChild(text);
   }
+
+  // Get results of optimization
+  let seasonPlan = getSeasonPlan(playerListItems, courtCount, subCount, dateArray.length, participantsPerMatchDay);
+
+  // Insert results in matchListTable
   for (i = 0; i < dateArray.length; i++) {
     row = matchListTable.insertRow(i + 1);
     cell = row.insertCell(0);
     let appendDate = document.createTextNode(dateArray[i].toDateString());
     cell.appendChild(appendDate);
-    for (j = 0; j < participantsPerMatch; j++) {
+    for (j = 0; j < participantsPerMatchDay; j++) {
       cell = row.insertCell(j + 1);
-      text = document.createTextNode("0");
+      text = document.createTextNode("0"); // Results here
       cell.appendChild(text);
-      console.log("Added item to table");
     }
   }
 }
---
+++ getLeastPlayedPlayers.js
@@ -0,0 +1,5 @@
+function getLeastPlayedPlayers(playersPlayedMatches, playersPlayingAtOnce) {
+  // This function returns a selected amount of players that have played
+  // the least amount of matches so far.
+  
+}

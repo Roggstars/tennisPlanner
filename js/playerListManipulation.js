a4748fd96516fc7adf233c53283021164706d1bb
jro <julian.roggendorf@rub.de>
2020-12-15 12:32:42 +0100

    Minor redesign

--- js/playerListManipulation.js
+++ js/playerListManipulation.js
@@ -2,12 +2,11 @@ function addItem() {
     // This function adds an item to the playerList.
     let li = document.createElement("LI");
     li.classList.add("list-group-item");
-    li.classList.add("list-group-item-action");
     let input = document.getElementById("playerToAdd");
     if (input.value !== "") {
-        li.innerHTML = input.value;
-        input.value = "";
+        li.appendChild(document.createTextNode(input.value));
         document.getElementById("playerList").prepend(li);
+        input.value = "";
     }
 }
 function removeItem() {
